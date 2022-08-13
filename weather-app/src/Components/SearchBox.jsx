c9b2e419d360356a2666d81f74d37637914ff818
Prakhar <prakhar@VT2499.bbrouter>
2022-08-13 16:34:16 +0530

    added SearchBox component

---
+++ weather-app/src/Components/SearchBox.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { UilSearch, UilLocationPoint} from "@iconscout/react-unicons";
+import Cities from "./Cities";
+
+function SearchBox({ setQuery, weatherData }) {
+  const [input, setInput] = useState(weatherData.name);
+  const [divToggle, setDivToggle] = useState(false);
+
+  const onSubmit = () => {
+    setQuery({ q: input });
+  };
+
+  return (
+    <div className="relative mt-5 w-full text-black md:mx-auto md:w-1/2">
+      <div className="relative flex flex-row items-center w-full text-black">
+        <UilLocationPoint
+          size={22}
+          className="absolute ml-5 text-blue cursor-pointer transition ease-out hover:scale-125"
+        />
+
+        <input
+          onClick={() => setDivToggle(true)}
+          onKeyDown={(event) => {
+            if(event.key == "Enter") {
+                onSubmit();
+            }
+          }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Search for city...."
+          id="input"
+          className="rounded-lg pl-10 text-xl font-light p-2 w-full h-12 capitalize placeholder:lowercase mx-4"
+        />
+
+        <UilSearch
+          onClick={onSubmit}
+          size={22}
+          className="absolute right-10 text-blue cursor-pointer transition ease-out hover:scale-125"
+        />
+      </div>
+      {divToggle ? (
+        <Cities
+          setInput={setInput}
+          input={input}
+          setDivToggle={setDivToggle}
+        />
+      ) : null}
+    </div>
+  );
+}
+
+export default SearchBox;
