24b42defa1c80ae6b5f230895ca07e594e2039c0
Prakhar <prakhar@VT2499.bbrouter>
2022-08-13 16:19:56 +0530

    added cities for debouncing

---
+++ weather-app/src/Components/Cities.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { nanoid } from 'nanoid'
+
+function Cities({ input, setInput, setDivToggle }) {
+  const [cities, setCities] = useState([
+    "Lucknow",
+    "Delhi",
+    "Mumbai",
+    "Bengaluru",
+    "Kanpur",
+    "Pune",
+    "Mohali",
+    "Noida",
+    "Surat",
+  ]);
+  return (
+    <div className="absolute z-10 bg-white mt-4 flex px-4 flex-col items-center my-2 w-full text-black">
+      <div className="rounded-lg flex flex-col w-full">
+        {cities
+          .filter((val) => {
+            if (val.toLowerCase().includes(input.toLowerCase())) {
+              return val;
+            }
+          })
+          .map((item, i) => {
+              let key = nanoid();
+            return <div
+              onClick={() => {
+                setDivToggle(false);
+                setInput(item);
+              }}
+              key={key}
+              className="h-12 flex items-center pl-2 bg-bg-div border-b border-b-color"
+            >
+              {item}
+            </div>
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default Cities;
