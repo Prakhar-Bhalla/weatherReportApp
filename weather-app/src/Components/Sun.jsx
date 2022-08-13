a5694ef04ef13600ea855e6d787e72cbccdff6b9
Prakhar <prakhar@VT2499.bbrouter>
2022-08-13 18:01:47 +0530

    added outer main box

--- weather-app/src/Components/Sun.jsx
+++ weather-app/src/Components/Sun.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-
+import { formatToLocalTime } from "../Modules/weather"
 
 function Sun({ weatherData: { timezone }, dailyDetails }) {
   return (
     <div className="flex flex-row w-full my-4 md:mx-auto">
       <div className="w-1/2 h-18 ml-2 mr-3">
         <p>Sunrice</p>
+        <p>{formatToLocalTime(dailyDetails.sunrise, timezone, "hh:mm a")}</p>
       </div>
       <div className="w-1/2 mr-2 h-18 text-right">
         <p>Sunset</p>
+        <p>{formatToLocalTime(dailyDetails.sunset, timezone, "hh:mm a")}</p>
       </div>
     </div>
   );
